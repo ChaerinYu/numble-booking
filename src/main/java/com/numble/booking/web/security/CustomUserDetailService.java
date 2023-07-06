5ba784f3c2d87b997e90a43a53b7309b7da19173
ChaerinYu <chaerin.du.ub@gmail.com>
2023-07-06 18:31:32 +0900

    spring security 수정

---
+++ src/main/java/com/numble/booking/web/security/CustomUserDetailService.java
@@ -0,0 +1,41 @@
+package com.numble.booking.web.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.numble.booking.user.domian.User;
+import com.numble.booking.user.exception.NotFoundUserException;
+import com.numble.booking.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * <pre>
+ * Class Name : CustomUserDetailService
+ * Description :
+ *
+ * Modification Information
+ * Modify Date      Modifier    Comment
+ * -------------------------------------------------------------
+ * 2023-07-06 	    chaerin 	New
+ * </pre>
+ *
+ * @author chaerin
+ * @since 2023-07-06
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByLoginId(username)
+                .orElseThrow(NotFoundUserException::new);
+        // TODO userLoginService, CustomUser 만들기
+        return null;
+    }
+}
--- src/main/java/com/numble/booking/web/security/SecurityConfig.java
+++ src/main/java/com/numble/booking/web/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.numble.booking.web.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * <pre>
@@ -23,21 +26,63 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
  * @since 2023-06-22
  */
 @Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig{
 
-    // spring security 필터 체인
-    @Bean
-    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http.csrf().disable()
-                .authorizeExchange()
-                .pathMatchers("/").permitAll()
-                .anyExchange().permitAll();
-        return http.build();
+    private final CustomUserDetailService customUserDetailsService;
+
+    public SecurityConfig(@Lazy CustomUserDetailService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    /**
+     * 스프링 시큐리티 규칙
+     * 보안 구성의 추가적인 설정을 수행
+     * 보안 구성의 세부사항을 구성하고, 필터 체인, 인증 및 권한 설정 등을 정의
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()                             // CSRF 비활성화
+                .headers().frameOptions().disable().and() // X-Frame-Options 비활성화
+                .formLogin().disable()                    // 폼 로그인 비활성화
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+//                .antMatchers("/public").permitAll()
+//                .antMatchers("/private").authenticated()
+//                .anyRequest().authenticated()
+                .anyRequest().permitAll();
+        return http.build();
+    }
+
+    /**
+     * 사용자 인증하는 방법이 담긴 객체
+     */
+    @Bean
+    public DaoAuthenticationProvider userDetailServices() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setHideUserNotFoundExceptions(false);
+        return authenticationProvider;
+    }
+
+    /**
+     * 스프링 시큐리티 룰을 무시하게 하는 url 규칙 (여기 등록하면 규칙 적용하지 않음)
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers(
+//                "/resources/**",
+//                "/css/**",
+//                "/favicon/**",
+//                "/img/**",
+                "/swagger*/**"
+        );
     }
 }
