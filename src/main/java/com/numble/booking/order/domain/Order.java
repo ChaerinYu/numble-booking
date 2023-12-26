9780bdf6cb90c7986a9d685340d4c1cd41ce57c4
ChaerinYu <chaerin.du.ub@gmail.com>
2023-12-26 17:50:36 +0900

    coupon 삭제

---
+++ src/main/java/com/numble/booking/order/domain/Order.java
@@ -0,0 +1,15 @@
+package com.numble.booking.order.domain;/** 
+ * <pre>
+ * Class Name : Order 
+ * Description : 
+ *
+ * Modification Information
+ * Modify Date      Modifier    Comment
+ * -------------------------------------------------------------
+ * 2023-12-26 	    chaerin 	New
+ * </pre>
+ *
+ * @author chaerin
+ * @since 2023-12-26
+*/public class Order {
+}
